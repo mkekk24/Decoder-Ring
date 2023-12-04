edd2aef6b31b23b4f161426ff7c861bc79357741
Michele <149628532+mkekk24@users.noreply.github.com>
2023-12-04 15:21:29 -0500

    Add files via upload
    REVISION

--- Project-_Decoder_ring-Michele_Kekkonen-Solution/scripts/polybius.js
+++ Project-_Decoder_ring-Michele_Kekkonen-Solution/scripts/polybius.js
@@ -3,56 +3,27 @@
 // Only add code (helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+function polybiusRenderer() {
+  const form = document.querySelector("#polybius");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-const encodeCypher = {
-    'a':'11', 'b':'21', 'c':'31', 'd':'41', 'e':'51',
-    'f':'12', 'g':'22', 'h':'32', 'i':'42', 'j':'42', 'k':'52',
-    'l':'13', 'm':'23', 'n':'33', 'o':'43', 'p':'53',
-    'q':'14', 'r':'24', 's':'34', 't':'44', 'u':'54',
-    'v':'15', 'w':'25', 'x':'35', 'y':'45', 'z':'55'
-  }
+    const input = event.target["polybius-input"].value;
+    const direction = event.target["polybius-options"].value;
+    const result =
+      direction === "encode"
+        ? polybiusModule.polybius(input)
+        : polybiusModule.polybius(input, false);
 
-  const decodeCypher = {
-    '11':'a', '21':'b', '31':'c', '41':'d', '51':'e',
-    '12':'f', '22':'g', '32':'h', '42':'(i/j)', '52':'k',
-    '13':'l', '23':'m', '33':'n', '43':'o', '53':'p',
-    '14':'q', '24':'r', '34':'s', '44':'t', '54':'u',
-    '15':'v', '25':'w', '35':'x', '45':'y', '55':'z'
-  }
-  function polybius(input, encode = true) {
-    if (encode) {
-      let conversion = input.toLowerCase();
-      let newCode = [...conversion].map((i) => {
-        if (i.charCodeAt() < 97 || i.charCodeAt() > 122) return i;
-        return encodeCypher[i];
-      });
-      return newCode.join("");
-
+    const alert = document.querySelector("#polybius-alert");
+    if (result) {
+      alert.classList.add("d-none");
+      const output = document.querySelector("#polybius-output");
+      output.innerHTML = result;
     } else {
-      if (Math.abs(input.split(" ").join("").length % 2) == 1) return false;
-      const answerArray = []
-      for(i=0;i<input.length;i=i+2){
-        let thisNum = input[i];
-        let nextNum = input[i+1];
-        if (thisNum === " ") {
-          answerArray.push(' ');
-          i=i-1;
-        } else {
-        answerArray.push(`${thisNum}${nextNum}`);
-        };
-      };
-      let newAnswer = answerArray.map((i) => {
-        if (i === ' ') return i;
-        return decodeCypher[i];
-      });   
-      return newAnswer.join('')
+      alert.classList.remove("d-none");
     }
-  }
-  return {
-    polybius,
-  };
-})();
+  });
+}
 
-module.exports = { polybius: polybiusModule.polybius };
+document.addEventListener("DOMContentLoaded", polybiusRenderer);
